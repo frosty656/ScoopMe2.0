95e38b29feac8f2b2e2267768ad17ecd0720d630
jacobfrost98@gmail.com <tjyTWK6L9gds>
2020-09-03 08:28:27 -0400

    Removed firebaseconfig from gitignore

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+      apiKey: "AIzaSyAlTimn115XF71BaqX3vUp32PYHh-F3Zjk",
+      authDomain: "scoopme-a3ae1.firebaseapp.com",
+      databaseURL: "https://scoopme-a3ae1.firebaseio.com",
+      projectId: "scoopme-a3ae1",
+      storageBucket: "scoopme-a3ae1.appspot.com",
+      messagingSenderId: "784476538345",
+      appId: "1:784476538345:web:720ce0a17c60e48440f778",
+      measurementId: "G-08R4YNWZ8K"
+}
